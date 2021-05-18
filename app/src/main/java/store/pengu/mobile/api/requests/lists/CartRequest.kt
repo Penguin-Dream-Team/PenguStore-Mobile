26bf545d59cddae67117ba27caa4d93257edab23
João Soares <jsoares.public@gmail.com>
2021-05-18 09:37:09 +0100

    ffs

--- app/src/main/java/store/pengu/mobile/api/requests/lists/CartRequest.kt
+++ app/src/main/java/store/pengu/mobile/api/requests/lists/CartRequest.kt
@@ -3,5 +3,5 @@ package store.pengu.mobile.api.requests.lists
 import store.pengu.mobile.data.CartProduct
 
 data class CartRequest(
-    val requests: List<CartProduct>
+    val cartItems: List<CartProduct>
 )
